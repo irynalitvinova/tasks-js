f58a4e79ce547661fa095e3de7b092f64a30aa53
Iryna Litvinova <lytvynlysko@gmail.com>
2022-10-26 14:40:50 +0300

    added solution to task loop statement --for..in

---
+++ js/loop-statement.js
@@ -0,0 +1,29 @@
+// Training JS #12: loop statement --for..in and for..of
+/*
+In lesson #11, we learned that the for loop can be used to traverse an array. 
+If we want to traverse an object, we can use a variant of the for: for..in. 
+This can traverse all the keys of the object. An example:
+function showObjectKeys(obj){
+  for (var key in obj){
+    console.log(key);
+  }
+}
+function showObjectValues(obj){
+  for (var key in obj){
+    console.log(obj[key]);
+  }
+}
+var ob={item1:"This",item2:"is",item3:"an",item4:"example"};
+console.log("keys of ob:")
+showObjectKeys(ob);
+console.log("values of ob:")
+showObjectValues(ob);
+*/
+function giveMeFive(obj) {
+  let itemFive = [];
+  for (let key in obj) {
+    if (key.length === 5) itemFive.push(key);
+    if (obj[key].length === 5) itemFive.push(obj[key]);
+  }
+  return itemFive;
+}
