e856659429079341d2c1c4ba673007fa2866eab7
Iryna Litvinova <lytvynlysko@gmail.com>
2022-12-10 16:47:53 +0200

    added solution to task multiply function

---
+++ js/multiply-function.js
@@ -0,0 +1,10 @@
+/*
+Implement a function called multiply, which takes two numbers and returns their product:
+multiply(2, 3) = 6
+multiply(0, 188) = 0
+multiply(85, 144) = 12240 
+*/
+function multiply(numOne, numTwo) {
+  return numOne * numTwo;
+}
+
