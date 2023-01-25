de48047b7732f39a2360ea1621deff1c13c33ca5
Iryna Litvinova <lytvynlysko@gmail.com>
2023-01-25 19:04:30 +0200

    added second solution to task quarter of the year

--- js/quarter-of-the-year.js
+++ js/quarter-of-the-year.js
@@ -10,3 +10,25 @@ and month 11 (November), is part of the fourth quarter.
 const quarterOf = (month) => {
   return Math.ceil(month/3);
 }
+const quarter = 0;
+const quarterOfYear = (month, quarter = 4) => {
+	switch (month) {
+		case 1:
+		case 2:
+		case 3:
+			quarter = 1;
+			break;
+		case 4:
+		case 5:
+		case 6:
+			quarter = 2;
+			break;
+		case 7:
+		case 8:
+		case 9:
+			quarter = 3;
+			break;
+	}
+	return quarter;
+};
+
