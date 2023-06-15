b2c8d834f3d4ebb9666fdd60401e900e7b747231
Iryna Litvinova <lytvynlysko@gmail.com>
2023-06-15 20:09:48 +0300

    Add task Conditional statement--switch How many days

--- js/loop.js
+++ js/loop.js
@@ -79,3 +79,22 @@ function check(arr, x) {
 }
 
 
+// Training JS #8: Conditional statement--switch
+// https://www.codewars.com/kata/572059afc2f4612825000d8a
+
+function howManydays(month) {
+  let days;
+  switch (month) {
+    case 2:
+      return 28;
+    case 4:
+    case 6:
+    case 9:
+    case 11:
+      return 30;
+    default:
+      return 31;
+  }
+}
+
+
