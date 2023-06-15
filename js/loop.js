269e91a65e5ca7db14ed521070cc0b4a48c69cfc
Iryna Litvinova <lytvynlysko@gmail.com>
2023-06-15 20:45:47 +0300

    Add task Training JS #11: loop statement --break,continue Grab Doll

--- js/loop.js
+++ js/loop.js
@@ -98,3 +98,18 @@ function howManydays(month) {
 }
 
 
+// Training JS #11: loop statement --break,continue
+// https://www.codewars.com/kata/5721c189cdd71194c1000b9b/train/javascript
+
+function grabDoll(dolls) {
+  let bag = [];
+  for (let i = 0; i < dolls.length; i++) {
+    if (dolls[i] === "Hello Kitty" || dolls[i] === "Barbie doll") {
+      bag.push(dolls[i]);
+      if (bag.length >= 3) break
+    } else continue;
+  }
+  return bag;
+}
+
+
